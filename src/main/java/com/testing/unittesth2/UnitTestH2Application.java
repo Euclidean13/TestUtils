5910d2b468d807e118b53e5f694d1e1e7b0c896d
Euclidean13 <aacristellys@icloud.com>
2020-05-20 21:49:24 +0200

    master: Set ups the project

---
+++ src/main/java/com/testing/unittesth2/UnitTestH2Application.java
@@ -0,0 +1,13 @@
+package com.testing.unittesth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitTestH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitTestH2Application.class, args);
+	}
+
+}
